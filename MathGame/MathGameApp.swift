8cabcfafd238fdb85cb492c60ae50ea42d1bffff
Shehzad Ahmed <shaxad.here@gmail.com>
2023-04-25 04:25:36 +0500

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Shehzad on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
